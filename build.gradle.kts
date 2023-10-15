a29dc1cf61d93fd572c6089be51298cef596e0b0
Tom <mail@tomjuri.de>
2023-10-15 02:59:38 +0200

    make ready for jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,23 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "de.tomjuri"
 version = "1.0.0"
 
+java.withSourcesJar()
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 java.targetCompatibility = JavaVersion.VERSION_1_8
 java.toolchain.languageVersion = JavaLanguageVersion.of(8)
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            groupId = group.toString()
+            artifactId = project.name.lowercase()
+            version = project.version.toString()
+        }
+    }
+}
